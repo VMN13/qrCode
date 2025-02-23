123cda1247e445779a1d7d40751f20560f9fe08f
VMN13 <Stayspaceflux@mail.com>
2025-02-23 10:19:19 +0300

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import './Appcssphone.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,10 +3,20 @@ import { Link } from 'react-router-dom';
 
 export const Navigation = () => {
   return (
-    <nav className="nav">
+    <>
+    <div>
+      <h1 className='title'>Navigate</h1>
+    <div className='main'>
+      <div>
+      
       <h1 id='navigate'>
-        Navigate:
+        Navigate
       </h1>
+    <iframe className='video' width="560" height="315" src="https://www.youtube.com/embed/k09ip9Z6TCk?si=b2qkfnj3qt6FRjfC&amp;controls=0&amp;start=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    <nav className="nav">
+      
+      
       <Link className='link' to="/qrCodeGenerator">Code Generator</Link>
       <br />
       <Link className='link' to={"/qrCodeScanner"}>Scanner Generator</Link>
@@ -14,7 +24,11 @@ export const Navigation = () => {
       <Link className='link' to="/scanHistory">History Scan</Link>
       <br />
       <Link className='link' to="./generateHistory">History Generator</Link>
+      
     </nav>
+    </div>
+    </div>
+    </>
 );
 };
 
